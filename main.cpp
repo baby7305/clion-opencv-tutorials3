ca012bc2bf818d154716e174fa4d9eb697c93d7d
baby7305 <songhaibei7305@163.com>
2019-03-04 13:46:29 +0800

    mingw编译器demo
    准备一个读取图片demo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Mat srcImage = imread("../lianggongchunri_de_youyv-009.jpg");
+    if (!srcImage.data) {
+        std::cout << "Image not loaded";
+        return -1;
+    }
+    imshow("[img]", srcImage);
+    waitKey(0);
     return 0;
 }
