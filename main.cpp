02de697ebb52fba82f24f59f6d7fc5ae3fc86ca9
baby7305 <songhaibei7305@163.com>
2019-07-31 20:27:19 +0800

    clang编译器demo
    准备一个读取图片demo。find_package(OpenCV REQUIRED)，加不加吧，Mac会自动找homebrew装好的OpenCV。

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Mat srcImage = imread("../WechatIMG31.jpeg");
+    if (!srcImage.data) {
+        std::cout << "Image not loaded";
+        return -1;
+    }
+    imshow("[img]", srcImage);
+    waitKey(0);
     return 0;
 }
